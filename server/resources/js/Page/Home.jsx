aef1d3b4aa1ffc0e5a007ddd2ef677d0de3c6547
Wesley Schreur <info@wesleyschreur.nl>
2023-03-15 12:30:52 +0100

    React-js uninstall

--- server/resources/js/Page/Home.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-
-	const heading = "Laravel 10 with Vite, Tailwind, React JS";
-
-	return <div> {heading}</div>;
-
-}
--- server/vite.config.js
+++ server/vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
 
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/app.jsx'
 
             ],
             refresh: true,
