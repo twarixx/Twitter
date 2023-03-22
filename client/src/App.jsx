3b2d09e12dc16125586a333d08c447bb126946df
twarixx <amberful@craftville.net>
2023-03-22 13:17:07 +0100

    Toaster messages instead of alert()

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,87 +1,97 @@
-import {useContext} from "react";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {AuthContext} from "./context/AuthContext.jsx";
-import {createBrowserRouter, Navigate, Outlet, RouterProvider} from "react-router-dom";
-import {HomePage} from "./pages/HomePage.jsx";
-import {LoginPage} from "./pages/auth/LoginPage.jsx";
-import {RegisterPage} from "./pages/auth/RegisterPage.jsx";
-import {UnknownPage} from "./pages/UnknownPage.jsx";
-import {MainLayout} from "./layouts/MainLayout.jsx";
+import { useContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthContext } from "./context/AuthContext.jsx";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import { HomePage } from "./pages/HomePage.jsx";
+import { LoginPage } from "./pages/auth/LoginPage.jsx";
+import { RegisterPage } from "./pages/auth/RegisterPage.jsx";
+import { UnknownPage } from "./pages/UnknownPage.jsx";
+import { MainLayout } from "./layouts/MainLayout.jsx";
 
 function App() {
-    const {currentUser} = useContext(AuthContext);
-    const queryClient = new QueryClient()
+  const { currentUser } = useContext(AuthContext);
+  const queryClient = new QueryClient();
 
-    function detectMobile() {
-        return (Math.min(window.screen.width, window.screen.height) < 768);
+  function detectMobile() {
+    return Math.min(window.screen.width, window.screen.height) < 768;
+  }
+
+  const LoggedIn = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
     }
 
+    return children;
+  };
 
-    const LoggedIn = ({children}) => {
-        if (!currentUser) {
-            return <Navigate to="/login"/>;
-        }
-
-        return children;
+  const LoggedOut = ({ children }) => {
+    if (currentUser) {
+      return <Navigate to="/" />;
     }
 
-    const LoggedOut = ({children}) => {
-        if (currentUser) {
-            return <Navigate to="/"/>;
-        }
+    return children;
+  };
 
-        return children;
-    }
-
-    const router = createBrowserRouter([
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <LoggedIn>
+          <MainLayout />
+        </LoggedIn>
+      ),
+      children: [
         {
-            path: "/",
-            element: (
-                <LoggedIn>
-                    <MainLayout/>
-                </LoggedIn>
-            ),
-            children: [
-                {
-                    path: "/",
-                    element: <HomePage/>,
-                },
-            ],
+          path: "/",
+          element: <HomePage />,
+        },
+      ],
+    },
+    {
+      path: "/",
+      element: (
+        <LoggedOut>
+          <Outlet />
+        </LoggedOut>
+      ),
+      children: [
+        {
+          path: "/login",
+          element: <LoginPage />,
         },
         {
-            path: "/",
-            element: (<LoggedOut><Outlet/></LoggedOut>),
-            children: [
-                {
-                    path: "/login",
-                    element: <LoginPage/>,
-                },
-                {
-                    path: "/register",
-                    element: <RegisterPage/>,
-                },
-            ],
+          path: "/register",
+          element: <RegisterPage />,
         },
-        {
-            path: "*",
-            element: <UnknownPage/>,
-        }
-    ]);
+      ],
+    },
+    {
+      path: "*",
+      element: <UnknownPage />,
+    },
+  ]);
 
-    if (!detectMobile()) {
-        return (
-            <div className="flex flex-col justify-center items-center h-[100vh]">
-                <img src="/images/logo.png" alt="Twiterr Logo"/>
-                <h1 className="text-3xl text-twitter-text font-semibold">We are mobile only!</h1>
-            </div>
-        )
-    }
-
+  if (!detectMobile()) {
     return (
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router}/>
-        </QueryClientProvider>
-    )
+      <div className="flex flex-col justify-center items-center h-[100vh]">
+        <img src="/images/logo.png" alt="Twiterr Logo" />
+        <h1 className="text-3xl text-twitter-text font-semibold">
+          We are mobile only!
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -1,64 +1,89 @@
-import {useContext, useState} from "react";
-import {AuthContext} from "../../context/AuthContext.jsx";
-import {useNavigate} from "react-router-dom";
-import {getAccounts} from "../../data/accounts.jsx";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { getAccounts } from "../../data/accounts.jsx";
+import { toaster } from "evergreen-ui";
 
 export const LoginPage = () => {
-    const [input, setInput] = useState({username: "", password: ""});
+  const [input, setInput] = useState({ username: "", password: "" });
 
-    const {login} = useContext(AuthContext);
-    const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        setInput({...input, [e.target.name]: e.target.value});
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    if (input.username === "" || input.password === "") {
+      toaster.danger("Please fill all the fields!", {
+        hasCloseButton: true,
+        duration: 3,
+      });
+      return;
     }
 
-    const handleLogin = (event) => {
-        event.preventDefault();
-
-        if (input.username === "" || input.password === "") {
-            alert("Please fill in all fields");
-            return;
-        }
-
-        const user = getAccounts().find(account => account.username === input.username);
-        if (!user) {
-            alert("User not found");
-            return;
-        }
-
-        login(user);
-        navigate("/");
+    const user = getAccounts().find(
+      (account) => account.username === input.username
+    );
+    if (!user) {
+      toaster.danger("Could not find this user!", {
+        hasCloseButton: true,
+        duration: 3,
+      });
+      return;
     }
 
-    return (
-        <div className="flex flex-col justify-center items-center h-[100vh]">
-            <div className="flex flex-col bg-stone-800 twitterr-shadow h-[60%] w-[85%] rounded-xl p-6 items-center">
-                <div className="mt-7 mb-10">
-                    <img src="/images/logo.png" alt="Twiterr Logo"/>
-                </div>
+    login(user);
+    toaster.success("Successfully logged in!", {
+      hasCloseButton: true,
+      duration: 3,
+    });
+    navigate("/");
+  };
 
-                <form className="justify-center flex flex-col gap-4 items-center w-full text-twitter-text">
-                    <input onChange={handleChange} value={input.username}
-                           className="border-twitter-primary border outline-none w-full p-2 rounded bg-stone-700"
-                           name="username" type="text" placeholder="Username or Email"/>
-                    <input onChange={handleChange} value={input.password}
-                           className="border-twitter-primary border outline-none w-full p-2 rounded bg-stone-700"
-                           name="password" type="password" placeholder="Password"/>
-
-                    <div className="mt-6 w-full flex">
-                        <button onClick={handleLogin}
-                                className="bg-twitter-primary px-4 py-2 w-full h-12 rounded-md text-twitter-text_secondary">Sign
-                            in!
-                        </button>
-                    </div>
-                </form>
-
-                <div className="flex mt-0.5 w-full justify-between text-twitter-text text-sm">
-                    <p onClick={() => navigate("/register")}>Don't have an account?</p>
-                    <p onClick={() => navigate("/register")}>Sign up!</p>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col justify-center items-center h-[100vh]">
+      <div className="flex flex-col bg-stone-800 twitterr-shadow h-[60%] w-[85%] rounded-xl p-6 items-center">
+        <div className="mt-7 mb-10">
+          <img src="/images/logo.png" alt="Twiterr Logo" />
         </div>
-    )
-}
+
+        <form className="justify-center flex flex-col gap-4 items-center w-full text-twitter-text">
+          <input
+            onChange={handleChange}
+            value={input.username}
+            className="border-twitter-primary border outline-none w-full p-2 rounded bg-stone-700"
+            name="username"
+            type="text"
+            placeholder="Username or Email"
+          />
+          <input
+            onChange={handleChange}
+            value={input.password}
+            className="border-twitter-primary border outline-none w-full p-2 rounded bg-stone-700"
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+
+          <div className="mt-6 w-full flex">
+            <button
+              onClick={handleLogin}
+              className="bg-twitter-primary px-4 py-2 w-full h-12 rounded-md text-twitter-text_secondary"
+            >
+              Sign in!
+            </button>
+          </div>
+        </form>
+
+        <div className="flex mt-0.5 w-full justify-between text-twitter-text text-sm">
+          <p onClick={() => navigate("/register")}>Don't have an account?</p>
+          <p onClick={() => navigate("/register")}>Sign up!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
