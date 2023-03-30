5d396da43a6e1b62200d7a0ea439e6c8fdd92838
Wesley Schreur <info@wesleyschreur.nl>
2023-03-30 13:10:29 +0200

    Added search api

--- server/routes/api.php
+++ server/routes/api.php
@@ -19,6 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('search', [UserController::class, 'search']);
+
 Route::post('login', [UserController::class, 'login']);
 
 Route::get('users', [UserController::class, 'index']);
