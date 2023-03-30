ba2b1aa77bf67f8eed7748260ab2c6999a97045f
Wesley Schreur <info@wesleyschreur.nl>
2023-03-30 10:30:18 +0200

    template models created/updated

--- server/app/Models/Post.php
+++ server/app/Models/Post.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
+class Post extends Model {
+
     use HasFactory;
+
+	protected $fillable = [
+
+		'user_id',
+		'post_id',
+		'message',
+		'image',
+		'deleted',
+
+	];
+
 }
--- server/app/Models/Report.php
+++ server/app/Models/Report.php
@@ -5,22 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model {
+class Report extends Model {
 	
     use HasFactory;
 
 	protected $fillable = [
 
-		'email',
-		'password',
-		'username',
-		'display_name',
-		'bio',
-		'banner',
-		'profile_picture',
-		'admin',
-		'deactivated_on',
+		'user_id',
+		'post_id',
+		'reason',
+		'comment',
 
 	];
-	
+
 }
