4e2c94633c03b32bea68ba07608d2802275aad91
Wesley Schreur <info@wesleyschreur.nl>
2023-03-31 10:36:16 +0200

    tempalte ready for use

--- server/app/Http/Controllers/PostController.php
+++ server/app/Http/Controllers/PostController.php
@@ -4,45 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PostController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+class PostController extends Controller {
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+	public function index() {
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+
+
+	}
+
+
+
+	public function show_by_user(string $id) {
+
+
+
+	}
+
+
+
+	public function store(Request $request) {
+
+
+
+	}
+
+
+
+	public function show(string $id) {
+
+
+
+	}
+
+
+
+	public function update(Request $request, string $id) {
+
+
+
+	}
+
+
+
+	public function destroy(string $id) {
+
+
+
+	}
+
+
+
 }
