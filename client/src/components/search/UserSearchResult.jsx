6aeb0aa9e407bbdbc7e3c2516ea45819187c57bb
twarixx <97103370@st.deltion.nl>
2023-04-12 16:34:11 +0200

    Added user icon to show its an user

--- client/src/components/search/UserSearchResult.jsx
+++ client/src/components/search/UserSearchResult.jsx
@@ -1,13 +1,22 @@
 import { Username } from "../user/Username";
 import { Avatar } from "../user/Avatar";
+import { BiUser } from "react-icons/bi";
 
 export const UserSearchResult = ({ user }) => {
     return (
-        <div className="flex gap-3 bg-stone-600 rounded-lg p-2 mx-1">
-            <Avatar user={user} isLarge />
-            <div className="flex flex-col items-start justify-center">
-                <Username user={user} />
-                <p className="text-xs text-stone-400 -mt-1">@{user.username}</p>
+        <div className="bg-stone-600 rounded-lg p-2 mx-1 flex justify-between">
+            <div className="flex gap-3">
+                <Avatar user={user} isLarge />
+                <div className="flex flex-col items-start justify-center">
+                    <Username user={user} />
+                    <p className="text-xs text-stone-400 -mt-1">
+                        @{user.username}
+                    </p>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center">
+                <BiUser className="w-8 h-8 text-stone-400 m-1" />
             </div>
         </div>
     );
