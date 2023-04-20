f00ca48b466a7048a1f4fc3f64581ca59ebe8451
twarixx <97103370@st.deltion.nl>
2023-04-20 12:43:58 +0200

    hooivork

--- client/src/components/user/Username.jsx
+++ client/src/components/user/Username.jsx
@@ -1,3 +1,4 @@
+import { BiFork } from "react-icons/bi";
 import { MdVerifiedUser } from "react-icons/md";
 
 export const Username = ({ user }) => {
@@ -5,7 +6,17 @@ export const Username = ({ user }) => {
         <div className="relative">
             <p className="text-xl font-semibold break">{user.display_name}</p>
 
-            {user.verified_on ? (
+            {user.admin === 1 ? (
+                <BiFork
+                    color="#67e8f9"
+                    className="absolute -top-1.5 -right-7 w-7 h-7"
+                    title={`{user.display_name} is an admin.`}
+                />
+            ) : (
+                ""
+            )}
+
+            {user.verified_on && user.admin === 0 ? (
                 <MdVerifiedUser
                     color="#67e8f9"
                     className="absolute -top-0.5 -right-5 w-5 h-5"
